09324fb57e8e5c6d8f953022cd6ea9a92d01e017
Greg Ledbetter <gsledbetter@gmail.com>
2014-10-08 15:42:43 -0400

    Add icons for tabs. Add History table view controller.

---
+++ TempMonitor/TempMonitor/HistoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryTableViewController.h
+//  TempMonitor
+//
+//  Created by jtq6 on 10/8/14.
+//  Copyright (c) 2014 CDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryTableViewController : UITableViewController
+
+@end
